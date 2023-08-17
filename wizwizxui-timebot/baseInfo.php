c164b1aa9c20bc200ec75472cc4cafe25af82c08
Maysam Gamini <MAYSAM.GAMINI@aristocrat.com>
2023-08-17 09:49:51 -0500

    Init

---
+++ wizwizxui-timebot/baseInfo.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(0);
+$botToken = ''; //Bot Token
+$dbUserName = ''; //phpmyadmin username
+$dbPassword = ''; //phpmyadmin pass
+$dbName = ''; //phpmyadmin dbname wiz wiz
+$botUrl = ''; //https://[your server domain or ip]/wizwizxui-timebot/
+$admin = ; // telegram admin id
+?>
